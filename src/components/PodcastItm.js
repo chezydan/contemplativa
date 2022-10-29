9299b01d3c198a7f270e1be45bdaa829046ee0f9
dan <chezydan@gmail.com>
2022-10-29 21:24:08 +0300

    all the imgs , img style:

--- src/components/PodcastItm.js
+++ src/components/PodcastItm.js
@@ -6,7 +6,7 @@ import { FaSpotify } from "react-icons/fa"
 function PodcastItm({itm}) {
   
   const [readMore, setReadMore] = useState(false);
-   const {id, title,link, desc, temas} =  itm;
+   const {id, title,link, desc, temas, img} =  itm;
    function handleMoreClick(){
         setReadMore(!readMore)
    }
@@ -16,13 +16,17 @@ function PodcastItm({itm}) {
                    
                     <h3 className='podcastTitle'>{title}</h3>
                     <div className='cartDetails'>
+                    <img src={img} alt="img" />
                     <a href={link}  target="_blank" > link al episodio <span className='iconspotify'>
                     {<FaSpotify  size={'1.9rem'} /> }  </span></a>
                     <p>{readMore? desc : desc.slice(0,154)+'. . .' }
                     <span className='readmore'
                     onClick={handleMoreClick }>
-                        {readMore? `leer menos` : `ampliar`}</span> </p>
+                        {readMore? `leer menos` : `ampliar`}</span>
+                         </p>
                     <p className='temas'> {temas!=="" && <p>temas: {temas} </p> }   </p>
+                   
+                   
                     </div>
                 </div> )
                 
--- src/data/podcastData.js
+++ src/data/podcastData.js
@@ -6,6 +6,7 @@ export const episodes=[
         de la experiencia humana allí donde no la vemos. Se mencionará la ilusion de \
         satisfacción como punto de partida para ver la fuente de sufrimiento. Sufrimiento como constelación  \
         conceptual en oposición a mero dolor o circunstancia en la metafora de la 'segunda flecha'- Tambien se sugerirá una práctica informal para identificar lo que es experiencia vivencial y lo que es concepto de una experiencia. " ,
+        img:"https://s3-us-west-2.amazonaws.com/anchor-generated-image-bank/production/podcast_uploaded_episode400/23229132/23229132-1665426086213-d6aa6f8d4402c.jpg",
          temas: "sufrimiento intoducción" },
 
          { id :2, 
@@ -14,6 +15,7 @@ export const episodes=[
             desc : "Recapitulación sobre el sufrimiento. Planteos sobre ciencia y tradición. \
 Práctica meditativa de conciencia de adentro hacia afuera-  \
 El porqué de la experiencia humana como  no satisfactoria del  ser humano como ser biológico."  ,
+img:"https://s3-us-west-2.amazonaws.com/anchor-generated-image-bank/production/podcast_uploaded_episode400/23229132/23229132-1665484824030-f0962c12977d6.jpg",
              temas: "sufrimiento" },
              { id :3, 
                 title :"La gran mentira de nuestro cerebro. Así es como funciona una promesa vacía", 
@@ -23,6 +25,7 @@ El porqué de la experiencia humana como  no satisfactoria del  ser humano como 
                 Revelamos en este capítulo la gran mentira de nuestro cerebro, el mecanismo de anticipación que en realidad nos anticipa en momentos previos y nos conduce a llevar la atención y los recursos hacia una situación determinada la cual posiblemente no vivenciemos tan claramente.\
                 Este mecanismo es parte de la explicación de la condición de insatisfacción en la que esta inmersa la experiencia humana.\
                 Al final describiremos que se puede hacer para poder liberarse de la tiranía de este mecanismo."  ,
+                img:"https://s3-us-west-2.amazonaws.com/anchor-generated-image-bank/production/podcast_uploaded_episode400/23229132/23229132-1665484945530-7f4834996a1e8.jpg",
                  temas: "" },
        { id :4, 
                     title :"Recomendaciones para una sesión meditativa formal", 
@@ -30,22 +33,26 @@ El porqué de la experiencia humana como  no satisfactoria del  ser humano como 
                     desc:"En la introductión se recapitulan las ideas con respecto al sufrimiento. Se tratará el tema de llevar una sesión meditativa y una rutina en sus distintos aspectos: tiempo, postura, resolución, firme determinación y recapitualción-\
                     Meditación formal o informal. La importancia de contactarse con la esperiencia de meditar desde cerca y llevar hacia la vida cotidiana este modo de funcionamiento de la conciencia.\
                     El episodio contiene un interludio meditativo.       ",
+                    img:"https://s3-us-west-2.amazonaws.com/anchor-generated-image-bank/production/podcast_uploaded_episode400/23229132/23229132-1665485148959-71189f849e376.jpg",
                      temas: "método práctica" },
        { id :5, 
                         title :"La Fuerza de la Fuerte Determinación", 
                         link :"https://open.spotify.com/episode/29JY2qirDGYVEaRFnN5XnK" ,
                         desc : "- Ejercicio meditativo de examinación conciente de sensaciones, estados y postura- La Fuerte Determinación ( o Firme Determinación)- Su importancia en la meditacion formal y en la vida cotidiana. Las virtudes de aceptación y ecuanimidad que permite con su práctica"  ,
+                        img:"https://s3-us-west-2.amazonaws.com/anchor-generated-image-bank/production/podcast_uploaded_episode400/23229132/23229132-1665485756840-373b3222652a4.jpg",
                          temas: "" },
        { id :6, 
                      title :"El episodio que hubiera preferido no hacer: el lado oscuro del pensamiento positivo", 
                             link :"https://open.spotify.com/episode/7L99qrx0WJC4DILX9dLmVY" ,
                             desc : "En el presente episodio: Cuál es la problemática del pensamiento positivo. Qué dice la tradición y cómo nos posicionamos frente a la realidad y eventualmente frente a un mejoramiento frente a la crisis y al sufrimiento. Aceptación, equanimidad, observación de la realidad material \
                             como propuesta activa y deliberada. Trataremos la idea del optimismo obligatorio y sus consecuencias para diferenciarlo de la actitud frente a la realidad sin distorsiones ni autoengaño. En este episodio hay tambien espacio para la práctica de la meditación sobre las sensaciones de la respiracion en un interludio meditativo."  ,
+                            img:"https://s3-us-west-2.amazonaws.com/anchor-generated-image-bank/production/podcast_uploaded_episode400/23229132/23229132-1665469007006-fe73790c18d18.jpg",
                              temas: "pensamiento " },
       { id :7, 
         title :"Contar las respiraciones, un método para apoyar la práctica de la meditación", 
                                 link :"https://open.spotify.com/episode/5MPfDlaGOvmHoG0KfLs803" ,
                                 desc : "Es sorprendente como el conocimiento de la experiencia de meditar nos provee de una descripción tan fiel de lo nos ayuda o nos obstaculiza el proceso de contemplar por medio de la meditación. En este episodio: El porqué de la respiración como objeto de meditación de contemplación pura. El método de contar las respiraciones, fundamentos y consignas para su aplicación. Y por supuesto, pondremos en práctica este método."  ,
+                                img:"https://s3-us-west-2.amazonaws.com/anchor-generated-image-bank/production/podcast_uploaded_episode400/23229132/23229132-1665469493476-1344fd1754c6d.jpg",
                                  temas: "práctica método" },                       
      { id :8, 
              title :"El tratamiento de los pensamientos automáticos. ¿Tiene sentido suprimir los pensamientos?", 
@@ -53,8 +60,9 @@ El porqué de la experiencia humana como  no satisfactoria del  ser humano como 
               desc : "Suprimir los pensmientos que nos distraen o molestan. El rol que esta actividad juega en el contexto de la observación de la realidad.\
               Nuestra experiencia conciente periférica-general y los pensamientos automáticos. El porqué de la ineficacia de esta táctica y sobre todo porqué constituye una contradicción a nuestra intención de observación comprometida con lo que aparece frente a nosotros. ¿Que hacer ante los pensamientos distractivos en la meditación con atención presente? La sesión práctica no faltará a la cita. Es sentimiento de percatarse de esos pensamientos automáticos con un 'gesto interno'\
                de que algo aparece allí, aunque no querramos prestarle atención en este momento."  ,
+               img:"https://s3-us-west-2.amazonaws.com/anchor-generated-image-bank/production/podcast_uploaded_episode400/23229132/23229132-1665467402434-48fc985722837.jpg",
               temas: "pensamientos_automáticos" },
-              { id :9, 
+         { id :9, 
                title :"Cancelar este episodio por favooooor. El efecto boomerang de los pensamientos, desenmascarado-", 
                link :"https://open.spotify.com/episode/6FX5K6j58wuTeI0nuJUgnL" ,
                desc : "En este episodio se repasarán los efectos de la supresión de pensamientos en la práctica de meditación.\
@@ -63,6 +71,7 @@ El porqué de la experiencia humana como  no satisfactoria del  ser humano como 
                Práctica-habito de meditación.\
                Se propone una sección de práctica con aprendizaje de como percibir los pensamientos y demás distracciones.\
                Se describen los mecanismos mentales que explican el fracaso de la supresión de pensamientos.   " ,
+               img:"https://s3-us-west-2.amazonaws.com/anchor-generated-image-bank/production/podcast_uploaded_episode400/23229132/23229132-1665467866120-349558e8cec73.jpg",
                 temas: "pensamientos_automáticos" },
 
      { id :10, 
@@ -73,6 +82,7 @@ El porqué de la experiencia humana como  no satisfactoria del  ser humano como 
                   La posibilidad de transformar este funcionamiento tanto el contenido, el modo de verlo como el proyecto más profundo de poder cambiar la red que funciona la gran mayoria del tiempo, la red o la modalidad de funcionamiento por defecto  a nivel cerebral y mental.\
                   En esto consiste el famoso 'recableado'.\
                   Una sesión de meditación donde también se orienta a percibir las distracciones de manera tal de no dejarse llevar por el tren de pensamientos y donde el mantenimiento de la atención no vaya a expensas de nuestra experiencia conciente general."  ,
+                  img:"https://s3-us-west-2.amazonaws.com/anchor-generated-image-bank/production/podcast_uploaded_episode400/23229132/23229132-1665483514650-f0f53d04d618a.jpg",
                    temas: "pensamientos_automáticos  neurodinamica" },
                   
                    { id :11, 
@@ -82,6 +92,7 @@ El porqué de la experiencia humana como  no satisfactoria del  ser humano como 
                      En el presente episodio mostramos como llevar la atención en esta zona, cómo es el tipo de atención presente y concentración en una zona delimitada cuando dentro de la consigna uno puede percibir toda la variedad de sensaciones que hay en esa zona.\
                      Expandiremos también acerca de las carácterísticas de la meditación en las sensaciones de la respiración en general.\
                      Se incluyen dos sesiones de prácticas incluyendo una del método en cuestión."  ,
+                     img:"https://s3-us-west-2.amazonaws.com/anchor-generated-image-bank/production/podcast_uploaded_episode400/23229132/23229132-1665426498450-98d497d1372e7.jpg",
                       temas: "práctica método" },
      { id :12, 
                         title :"Explicando y describiendo la imperante dinámica del parloteo mental", 
@@ -90,6 +101,7 @@ El porqué de la experiencia humana como  no satisfactoria del  ser humano como 
                         Una breve práctica de meditación sobre las sensaciones de la respiración en la zona delimitada del triángulo. \
                         A continuación se explicará el mecanismo por el cual los pensamientos automáticos funcionan y porqué tienen tanto poder y se nos imponen con tanta fuerza.\
                         'Malas y buenas noticias'...  sobre la inercia de estos pensamientos y cuál es el camino hacia la transformación."  ,
+                        img:"https://s3-us-west-2.amazonaws.com/anchor-generated-image-bank/production/podcast_uploaded_episode400/23229132/23229132-1665425906942-0b41700c9222c.jpg",
                         temas: "práctica pensamientos" },
 
      { id :13, 
@@ -98,6 +110,7 @@ El porqué de la experiencia humana como  no satisfactoria del  ser humano como 
                           desc : "Nombrar o rotularlos pensamientos es un método conocido para consolidar la actividad meditativa.\
                           Especialmente en el trato de la observación de pensamientos, o de notarlos, sea que querramos redirigir la atención hacia un estímulo en el cual nos concentramos, o que la consigna sea observar simplemente los objetos de la mente sin selección.\
                           El episodio desarrolla el transfondo de como aplicar e integrar esta ayuda para  la meditación en la práctica."  ,
+                          img:"https://s3-us-west-2.amazonaws.com/anchor-generated-image-bank/production/podcast_uploaded_episode400/23229132/23229132-1665425493149-4a0612d6a8785.jpg",
                            temas: "método práctica pesamientos" },
               
       { id :14, 
@@ -107,6 +120,7 @@ El porqué de la experiencia humana como  no satisfactoria del  ser humano como 
        Cómo tratarlos, cómo considerarlos. Se hará una práctica de conciencia de la respiración en la zona delimitada del 'triángulo'. \                                          \
          Los temas de los cuales se ocupan los pensamientos automáticos van desde la nostalgia de un pasado irrecuperable hasta fantasía futura prometedora pasando por la posibilidad de una realidad alternativa, ocurrencias sobre nuestra autoimagen y los pensamientos y sentimientos de nuestro lugar en el grupo social.\
    En el episodio conóceremos sus contenidos para entenderlos, mientras que lo que hacemos en la meditación es contemplarlos no por sus contenidos si no por su sustrato material. "  ,
+   img:"https://s3-us-west-2.amazonaws.com/anchor-generated-image-bank/production/podcast_uploaded_episode400/23229132/23229132-1665425214381-a85fa30bbd706.jpg",
                              temas: "pensamientos" },
    { id :15, 
 
@@ -118,6 +132,7 @@ En el capítulo práctico, una exploración meditativa con transiciones por sens
 Actitud de aceptación frente  a las tendencias y forma de funcionamiento de los pensamientos automáticos y reacciones.\
 Aceptación como esfuerzo y postura activa y no mera rendición u omisión de acción.\
 Meditación y vida cotidiana con aceptación."  ,
+img:"https://s3-us-west-2.amazonaws.com/anchor-generated-image-bank/production/podcast_uploaded_episode400/23229132/23229132-1665424485704-d09aebc2a9b78.jpg",
  temas: "aceptación realidad autoengaño sufrimiento" },
 
    { id :16, 
@@ -125,6 +140,7 @@ title :"Notar las etapas de la respiración a través del seguimiento",
 link :"https://open.spotify.com/episode/1XT8dWSs0Ly6aOhXrrCQKf" ,
 desc : "En un nuevo episodio metodológico con ayudas para apoyar la práctica meditativa: Notar las etapas de la respiración a través del seguimiento. \
 Esta práctica nos reubica y nos ayuda a anclarnos o reanclarnos en el proceso de respiración en cuyas sensaciones estamos atendiendo y en realidad como microcosmos de toda acción a la que queremos obser var en su substrato material, en sus aspectos no relacionados con el contenido "  ,
+img:"https://s3-us-west-2.amazonaws.com/anchor-generated-image-bank/production/podcast_uploaded_episode400/23229132/23229132-1665424242158-25c08e8d1f3fd.jpg",
  temas: "método práctica " },
 
  { id :17, 
@@ -134,6 +150,8 @@ Esta práctica nos reubica y nos ayuda a anclarnos o reanclarnos en el proceso d
   La dificultad de poder contemplar en el presente ante la constante influencia de los pensamientos apremientes. \
   Su relación con el sufrimiento   La inmediatez del momento presente y de cómo la práctica meditativa se lleva por sobre todas las cosas de momento  a momento. \
     "  ,
+
+    img:"https://s3-us-west-2.amazonaws.com/anchor-generated-image-bank/production/podcast_uploaded_episode400/23229132/23229132-1665423496679-811e7f59af371.jpg",
    temas: "presente práctica " },
 
    { id :18, 
@@ -141,6 +159,7 @@ Esta práctica nos reubica y nos ayuda a anclarnos o reanclarnos en el proceso d
     link :"https://open.spotify.com/episode/6GWz8dCC8TtqcxAu7EvFAP" ,
     desc : "Entendiendo porqué el presente es un obstaculo. Lo vamos a entender por el conocimiento de como funciona la mente y por nuestra experiencia misma.\    Se nos hace difícil la experiencia de momento a momento parecemos padecer de un Sindrome de Intolerancia al Presente.\\   De cuáles son las llaves para salir de la evitación de la experiencia presente y \
     cuál es el camino para involucrarse hacia el cambio-"  ,
+    img:"https://s3-us-west-2.amazonaws.com/anchor-generated-image-bank/production/podcast_uploaded_episode400/23229132/23229132-1665486775425-6b7b874756019.jpg",
      temas: "presente" },
 
 
@@ -152,6 +171,7 @@ Esta práctica nos reubica y nos ayuda a anclarnos o reanclarnos en el proceso d
       Una visión ecuánime de todo momento implica que no hay tal jerarquía y que ningún momento es un medio para otro, entonces 'bajamos a tierra' y empezamos a vivir el presente.\
       Nuestro modo cotidiano de enfocar la experiencia presente es sofocándola viéndola como medio como recurso para otro momento y no en sí. Es decir estamos literalmente matando el tiempo.\
       La tradición contemplativa tiene otra propuesta para nosotros..."  ,
+      img:"https://s3-us-west-2.amazonaws.com/anchor-generated-image-bank/production/podcast_uploaded_episode400/23229132/23229132-1665422809748-a714febf26b69.jpg",
        temas: "presente aceptación" },
        { id :20, 
         title :"Presente actitud, intención y consecuencia", 
@@ -184,6 +204,7 @@ Esta práctica nos reubica y nos ayuda a anclarnos o reanclarnos en el proceso d
 title :"", 
 link :"" ,
 desc : ""  ,
+img:"" ,
  temas: "" },
 
  */
